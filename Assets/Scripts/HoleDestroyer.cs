e50904101ecf274ca18158ac4a49c49d4a98873f
SureshPanyala <sureshreddy9344@gmail.com>
2022-03-16 11:42:43 +0530

    Update HoleDestroyer.cs

--- Assets/Scripts/HoleDestroyer.cs
+++ Assets/Scripts/HoleDestroyer.cs
@@ -5,20 +5,7 @@ using UnityEngine.UI;
 
 public class HoleDestroyer : MonoBehaviour
 {
-   
-    // Start is called before the first frame update
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
+  private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Cointag"))
         {
